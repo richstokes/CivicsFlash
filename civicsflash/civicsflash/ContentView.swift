c9c0d1cea371712096c6fa1469794c9e20f2d2d6
Rich Stokes <rich@rsmail.co>
2025-11-22 08:27:55 -0800

    Adding qs

---
+++ civicsflash/civicsflash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  civicsflash
+//
+//  Created by Richard Stokes on 11/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ civicsflash/civicsflash/civicsflashApp.swift
@@ -0,0 +1,17 @@
+//
+//  civicsflashApp.swift
+//  civicsflash
+//
+//  Created by Richard Stokes on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct civicsflashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
